77e53809e779ca78a221a8322091e3ac4acba123
Christoffer <zbcchhan11@zbc.dk>
2017-05-17 21:46:00 +0200

    Added functions regarding assigning ip to interface
    Working
    - Get-NetIntIndex
    Still broken
    - Set-Ipv4Address
    - Set-EnableDHCPInterface

--- functions.ps1
+++ functions.ps1
@@ -10,11 +10,57 @@ function set_mailbox_quota($identity, $issueWarningQuota, $prohibitSendQuota, $p
     $prohibitSendReceiveQuota -UseDatabaseQuotaDefaults $useDatabaseQuotaDefaults
 }
 
+
+# Return NIC Index value from given NIC Interface Name
+function Get-NetIntIndex($intName) {
+    $intIndex = Get-NetAdapter -physical | 
+        where name -eq $intName | select -expand ifIndex
+
+    if ($intIndex.length -eq 0) {
+        Write-Host "Sadly. No Interface was found IP Enabled with the given Interface Name."
+        break;
+    }
+
+    return $intIndex
+}
+
+
 # Set IPv4 Address
-function set_ipv4_address() {}
+function Set-Ipv4Address($intName, $ipv4, $netmask, $gateway, $dns) {
+    $adapter = Get-WmiObject win32_NetworkAdapterConfiguration | `
+        Where Index -eq (Get-NetIntIndex -intName $intName)
+
+    $adapter.EnableStatic($ipv4, $netmask)
+    Sleep -Seconds 4
+    $adapter.SetGateways($gateway, 1)
+    $adapter.SetDNSServerSearchOrder($dns)
+}
+Set-Ipv4Address -intName "Ethernet" -ipv4 "192.0.2.2" `
+    -netmask '255.255.255.0' -gateway '192.0.2.1' -dns '192.0.2.254'
+
 
 # Set IPv6 Address
-function set_ipv6_address() {}
+function Set-Ipv6Address() {}
+
+
+# Set Network Interface mode to DHCP Enabled for both IPv4 and IPv6
+# and get DNS servers, too, with DHCP IP Address
+function Set-EnableDHCPInterface($intName, $setDnsToAuto = $true) {
+    if($intName.length -eq 0) {
+        Write-Host 'ERROR: A Network Intrface Name was not given.'
+        break
+    }
+
+    $adapter = Get-WmiObject win32_NetworkAdapterConfiguration | `
+        Where Index -eq (Get-NetIntIndex -intName $intName)
+    
+    $adapter.SetDNSServerSearchOrder()
+    $adapter.EnableDHCP()
+
+    $adapter.SetGateways()
+    
+    $adapter.SetDNSDomain()
+}
 
 
 # Create AD User
