4992e80318b500763ba6e2226ffb2858d9539667
Christoffer <zbcchhan11@zbc.dk>
2017-05-17 13:11:30 +0200

    Mostly pseudo kode for functions

---
+++ functions.ps1
@@ -0,0 +1,48 @@
+﻿# Create use cli menu
+
+
+# Define Disk Quotas
+function setMailboxQuota($identity, $issueWarningQuota, $prohibitSendQuota, $prohibitSendReceiveQuota, $useDatabaseQuotaDefaults) {
+    Set-Mailbox -Identity $identity -IssueWarningQuota $issueWarningQuota `
+    -ProhibitSendQuota $prohibitSendQuota -ProhibitSendReceiveQuota `
+    $prohibitSendReceiveQuota -UseDatabaseQuotaDefaults $useDatabaseQuotaDefaults
+}
+
+# Set IPv4 Address
+function setIpv4Address() {}
+
+# Set IPv6 Address
+function setIpv6Address() {}
+
+
+# Create AD User
+
+
+# Modify existing AD User
+
+
+# Create GPO
+
+
+# Link GPO to OU
+
+
+# Show exsiting AD Groups
+
+
+# Show members of AD Group
+
+
+# Show current disk usage
+
+
+# Show latest x warnings and errors from eventlog
+
+
+# Start services (defualt only services set to auto-startup)
+
+
+# Install AD
+
+
+# Configure AD
