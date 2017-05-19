3988bdbc182009737a8c4e87ce479abfd050fa2f
chhan11 <zbcchhan11@zbc.dk>
2017-05-19 15:11:39 +0200

    Fix logformat $timestamp

--- Functions-CustomLog.psm1
+++ Functions-CustomLog.psm1
@@ -1,5 +1,5 @@
 Function New-LogCustomEvent ($LogFile,$Success,$LogMessage,$Server) {
-    $Time = (((Get-Date -Format o).Split("{+}")[0]).Split("{.}"))
+    $Time = (((Get-Date -Format o).Split("{+}")[0]).Split("{.}")[0])
     $TimeZone = ((Get-Date -Format o).Split("{+}")[1])
     
     ("$Time+$TimeZone" `
