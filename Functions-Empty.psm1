de4e6c757f0df8f480c27f3dc645852896aea05f
chhan11 <zbcchhan11@zbc.dk>
2017-05-19 14:41:19 +0200

    Fixed Non-working function New-CsvADUsers

---
+++ Functions-Empty.psm1
@@ -0,0 +1,25 @@
+# Modify existing AD User
+
+
+# Create GPO
+
+
+# Link GPO to OU
+
+
+# Show exsiting AD Groups
+
+
+# Show members of AD Group
+
+
+# Show current disk usage
+
+
+# Show latest x warnings and errors from eventlog
+
+
+# Install AD
+
+
+# Configure AD
